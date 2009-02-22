e50b7e41e5f41537e4791604979b905fd75866fe
Richard Wallis <richard.wallis@dataliberate.com>
2009-02-22 00:59:06 +0000

    Initial checkin of codeline

---
+++ panels/juiceListPanel.js
@@ -0,0 +1,42 @@
+/*
+ * JuiceBasicPanel 0.1 - Javascript User Interface Componentised Extension
+ * http://juice-project.googlecode.com
+ *
+ * Copyright (c) 2009 Talis (talis.com)
+ * Originator: Richard Wallis
+ * Under GPL (gpl-2.0.txt) license.
+ *
+ * $Author$
+ * $Date$
+ * $Rev$
+ */
+function JuiceListPanel(insertDiv, panelId, startClass, liveClass, showFunc){
+	JuiceListPanel.superclass.init.call(this,insertDiv, panelId, startClass, liveClass, showFunc);
+	this.added = false;
+}
+
+JuiceListPanel.prototype = new JuicePanel();
+JuiceListPanel.prototype.constructor = JuiceListPanel;
+JuiceListPanel.superclass = JuicePanel.prototype;
+
+JuiceListPanel.prototype.add = function(sel){
+	this.insert();
+	this.show();
+	if(!this.added){
+		this.buildlist();
+		this.added = true;
+	}
+//	var htm = '<li title="'+ sel.selText() + '" id="' + this.getPanelId() + sel.processId() + '" class="' + this.startClass() + '" >';
+//	htm += sel.selText() + '</li>';
+	var htm = '<li title="'+ sel.selText() + '" id="' + this.getPanelId() + sel.processId() +  '" class="' + this.startClass() + '" >';
+	htm += '<a href="javascript:void(0)" title="'+ sel.selText() + '">';
+	htm += sel.selText() + '</a></li>';
+	
+	
+	$("#"+this.getPanelId()+" ul").append(htm);
+}
+
+JuiceListPanel.prototype.buildlist = function(){
+	var htm = '<ul class="JuiceList"></ul>';
+	$("#"+this.getPanelId()).append(htm);
+}
