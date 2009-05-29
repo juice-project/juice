dffcc475dbf4f05576e499cb4e2afd7a416cdc0e
sieheauch <sieheauch@3cf05578-ff67-11dd-9d1a-b59b2e1864b6>
2009-05-29 12:50:31 +0000

    branch should have been a tag instead

--- branches/20090529-voj-fix-branch-start/metadefs/duke_edu_metadef.js
+++
@@ -1,6 +0,0 @@
-function duke_edu_metadef(){
-	juice.addMeta(new JuiceMeta("isbn","li:has(> em:contains('ISBN:'))"));
-	juice.addMeta(new JuiceMeta("isbns","li:has(> em:contains('ISBN:'))",juice.stringToAlphnumAray));
-	juice.addMeta(new JuiceMeta("author","[title*='Perform a search for the author']"));
-	juice.addMeta(new JuiceMeta("title",".resourcedetails > h1"));
-}
