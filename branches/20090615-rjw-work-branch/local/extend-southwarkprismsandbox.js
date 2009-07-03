64576c0b20f7a750b2082a1cdc6f4c2eaa7795a1
richard.wallis@talis.com <richard.wallis@talis.com@3cf05578-ff67-11dd-9d1a-b59b2e1864b6>
2009-07-03 15:18:57 +0000

    taking local stuff out'

--- branches/20090615-rjw-work-branch/local/extend-southwarkprismsandbox.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	juice.setDebug(false);
-	juice.loadJs("http://talis-juice-live.s3.amazonaws.com/extensions/GoogleAnalytics.js");
-	juice.onJsLoaded(runExtensions);
-});
-
-function runExtensions(){
-	var procGas = new gasJuice(juice,"UA-2411194-14");
-}
