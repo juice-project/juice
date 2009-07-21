96a1ef1dd5d49072570870c92f7947f2254f6136
richard.wallis@talis.com <richard.wallis@talis.com@3cf05578-ff67-11dd-9d1a-b59b2e1864b6>
2009-07-21 15:09:42 +0000

    RJW Work branch

---
+++ branches/20090721-rjw-wrk02/metadefs/koha_metadef.js
@@ -0,0 +1,33 @@
+function koha_metadef(){
+	juice.addMeta(new JuiceMeta("isbns",".unapi-id","title",koha_scrapeunapi)); 
+	juice.addMeta(new JuiceMeta("author",".author  a")); 
+	juice.addMeta(new JuiceMeta("title","#catalogue_detail_biblio  h1")); 
+//	juice.debugMeta();
+}
+
+function koha_scrapeunapi(val,meta){
+	var value = null;
+	var id = meta.getId();
+	switch(id){
+		case "isbn":
+		case "isbns":
+			value = getFromParam(val,"koha:isbn");
+//			juice.debugOutln("ISBN: "+val);
+			break;
+		default:
+			break;
+		
+	}
+	return juice.stringToAlphnumAray(unescape(value));
+}
+
+function getFromParam(string,name){
+	var regexS = name+":([^&#]*)";
+	var regex = new RegExp( regexS );
+	var results = regex.exec( string );
+	  
+	if( results == null )
+	    return "";
+	 else
+	    return results[1];
+}
---
+++ branches/20090721-rjw-wrk02/panels/juiceBasicPanel.js
@@ -0,0 +1,19 @@
+/*
+ * JuiceBasicPanel 0.1 - Javascript User Interface Componentised Extension
+ * http://juice-project.googlecode.com
+ *
+ * Copyright (c) 2009 Talis (talis.com)
+ * Originator: Richard Wallis
+ * Under GPL (gpl-2.0.txt) license.
+ *
+ * $Author$
+ * $Date$
+ * $Rev$
+ */
+function JuiceBasicPanel(insertDiv, panelId, startClass, liveClass, showFunc){
+	JuiceBasicPanel.superclass.init.call(this,insertDiv, panelId, startClass, liveClass, showFunc);
+}
+
+JuiceBasicPanel.prototype = new JuicePanel();
+JuiceBasicPanel.prototype.constructor = JuiceBasicPanel;
+JuiceBasicPanel.superclass = JuicePanel.prototype;
