2f8aa82af8db50711b02cf5d13e3c448f8c9a939
Richard Wallis <richard.wallis@dataliberate.com>
2009-07-23 15:01:08 +0000

    Merge from 20090721-rjw-wrk02 for ver 0.6 - Meta refactor

--- metadefs/talis_prism_metadef.js
+++ metadefs/talis_prism_metadef.js
@@ -1,8 +1,8 @@
 function talis_prism_metadef(){
-	juice.addMeta(new JuiceMeta("isbns",".item > #details .table .ISBN",juice.stringToAlphnumAray));
-	juice.addMeta(new JuiceMeta("isbn",".item > #details .table .ISBN"));
-	juice.addMeta(new JuiceMeta("author",".item > .summary .author > .author"));
-	juice.addMeta(new JuiceMeta("title",".item > .summary > .title"));
-	juice.addMeta(new JuiceMeta("shelfmark","#availability > table > tbody > tr > td:nth-child(3)",null));
-	juice.addMeta(new JuiceMeta("location","#availability > table > tbody > tr > td:nth-child(2)",null));
+	juice.findMeta("isbns",".item > #details .table .ISBN",juice.stringToAlphnumAray);
+	juice.findMeta("isbn",".item > #details .table .ISBN");
+	juice.findMeta("author",".item > .summary .author > .author");
+	juice.findMeta("title",".item > .summary > .title");
+	juice.findMeta("shelfmark","#availability > table > tbody > tr > td:nth-child(3)",null);
+	juice.findMeta("location","#availability > table > tbody > tr > td:nth-child(2)",null);
 }
