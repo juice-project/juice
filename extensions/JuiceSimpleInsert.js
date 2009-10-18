0ada6a5153ef258472e040b40d810291b6e7c23d
Richard Wallis <richard.wallis@dataliberate.com>
2009-10-18 16:03:54 +0000

    version 0.6.1

--- extensions/JuiceSimpleInsert.js
+++ extensions/JuiceSimpleInsert.js
@@ -4,10 +4,12 @@
 //Constructor arguments:
 //arg: ju - instance of juice
 //arg: insert - JuiceInsert for page
+//arg: func - function to call after insert has been shown
 
-function simpleInsertJuice(ju,insert){
-	id = "simpleInsertSel";
-	initFunc = this.startsimpleInsert;
+function simpleInsertJuice(ju,insert,func){
+	var id = "simpleInsertSel";
+	var initFunc = this.startsimpleInsert;
+	this.func = func;
 	if(arguments.length){
 		simpleInsertJuice.superclass.init.call(this,id,initFunc,null,insert,ju);
 		simpleInsertJuice.superclass.startup.call(this);
@@ -21,5 +23,9 @@ simpleInsertJuice.superclass = JuiceProcess.prototype;
 
 simpleInsertJuice.prototype.startsimpleInsert = function(){
 	this.showInsert();
+	if(this.func){
+		this.func();
+	}
+	
 }
 
