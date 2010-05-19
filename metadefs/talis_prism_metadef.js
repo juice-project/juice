7a01619f7ceb3326ea46abc2abeee399fbb10106
Matt <mattmachell@users.noreply.github.com>
2010-05-19 13:27:18 +0000

    Fixed metadefs first pass.

--- metadefs/talis_prism_metadef.js
+++ metadefs/talis_prism_metadef.js
@@ -1,15 +1,16 @@
 function talis_prism_metadef(){
-	juice.findMeta("isbns",".item > #details .table .ISBN",juice.stringToAlphnumAray);
-	juice.findMeta("isbn",".item > #details .table .ISBN");
-	juice.findMeta("author",".item > .summary .author > .author");
-	juice.findMeta("title",".item > .summary > .title");
-	juice.findMeta("shelfmark","#availability > table > tbody > tr > td:nth-child(3)",null);
-	juice.findMeta("location","#availability > table > tbody > tr > td:nth-child(2)",null);
-
-	juice.findMeta("workids",".item > .summary > .title > a","href",talis_prism_items_workids);
-	juice.setMeta("workid",talis_prism_item_workid);
+	juice.findMeta("isbns",".item #details .table .ISBN",juice.stringToAlphnumAray);
+	juice.findMeta("isbn",".item #details .table .ISBN");
+	juice.findMeta("author",".item .summary .author .author");
+	juice.findMeta("title",".item .summary .title");
 	
-//	juice.debugMeta();
+	juice.findMeta("shelfmark","#availability .options table td:nth-child(2)",null);	
+	juice.setMeta("shelfmark",talis_prism_dedup(juice.getValues('shelfmark')));
+	
+	juice.findMeta("location","#availability  .options h3 span",null);
+
+	juice.findMeta("workids",".item .summary > .title > a","href",talis_prism_items_workids);
+	juice.setMeta("workid",talis_prism_item_workid);
 }
 
 function talis_prism_item_workid(){
@@ -30,3 +31,15 @@ function talis_prism_items_workids(val,id){
 		
 	}
 }
+
+function talis_prism_dedup(a) {
+	var i, r=[], o={};
+
+	for (i=0;i<a.length;i++) {
+		if (!o[a[i]]){
+		o[a[i]]={};
+		r.push(a[i]);
+		}
+	}
+	return r;
+	}
