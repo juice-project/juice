2aad2ff7576685eb2383fb322ae0ef7c5392bf4a
Matt <mattmachell@users.noreply.github.com>
2010-12-06 15:21:51 +0000

    juice moved to non-prototype object

--- tests/unit/loading.js
+++ tests/unit/loading.js
@@ -1,24 +1,28 @@
 module("juice.scriptloading");
 
-test("test script loading", function(){
-	expect(1);
-	stop(1000);
-	juice.loadJs('../extensions/delicious.js','', function(){
-													equals(typeof deliciousJuice, 'function', 'There should be a delicious extension');
-													start();
-													});
-});
+
 
 test("test multi script loading", function(){
+	
+	stop(600);
 	expect(2);
-	stop();
 	juice.loadExtensions('AspireList', 'copac');
 	setTimeout(function(){
-						equals(typeof AspireListJuice, 'function', 'There should be an aspire list extension');
-						equals(typeof copacJuice, 'function', 'There should be a copac extension');
 						
+						equal(typeof AspireListJuice, 'function', 'There should be an aspire list extension');
+						equal(typeof copacJuice, 'function', 'There should be a copac extension');
 						start();
-						}, 400);
+						}, 500);
 						
 
+});
+
+test("test script loading", function(){
+	stop();
+	juice.loadJs('../extensions/delicious.js','', function(){
+													
+													equal(typeof deliciousJuice, 'function', 'There should be a delicious extension');
+													
+													start();
+													});
 });
